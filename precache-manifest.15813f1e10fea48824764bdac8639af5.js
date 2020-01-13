667ea14e20cbe564d0ddce872181e9490848a9c2
Idahosa Akioya <akioyaidahosa@yahoo.com>
2020-01-13 14:42:37 +0100

    Updates

---
+++ precache-manifest.15813f1e10fea48824764bdac8639af5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c30031abb1558063ca925b9b7902b6",
+    "url": "/GetProspa/index.html"
+  },
+  {
+    "revision": "cae8f2b78258dbf7c91a",
+    "url": "/GetProspa/static/css/main.fd0fa12d.chunk.css"
+  },
+  {
+    "revision": "7d451a50316673280345",
+    "url": "/GetProspa/static/js/2.6b2c5b69.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/GetProspa/static/js/2.6b2c5b69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cae8f2b78258dbf7c91a",
+    "url": "/GetProspa/static/js/main.9291df30.chunk.js"
+  },
+  {
+    "revision": "22660dc285e3b369603e",
+    "url": "/GetProspa/static/js/runtime-main.8e96c548.js"
+  },
+  {
+    "revision": "2fe2decda52756fb809610140f05543d",
+    "url": "/GetProspa/static/media/gilroy-extrabold-webfont.2fe2decd.woff2"
+  },
+  {
+    "revision": "5b722a4fefd4dd8fa7f48e35a528c696",
+    "url": "/GetProspa/static/media/gilroy-extrabold-webfont.5b722a4f.woff"
+  },
+  {
+    "revision": "21d7684a43d6b4ffb4a1be2f65c62191",
+    "url": "/GetProspa/static/media/gilroy-light-webfont.21d7684a.woff2"
+  },
+  {
+    "revision": "966bee11289c250c0b95e34df92cc1dd",
+    "url": "/GetProspa/static/media/gilroy-light-webfont.966bee11.woff"
+  }
+]);
