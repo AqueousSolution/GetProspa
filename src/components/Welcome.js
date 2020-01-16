196ce8c9f81566ab4b5c31841b7cb9dfa81d72c2
Idahosa Akioya <akioyaidahosa@yahoo.com>
2020-01-16 19:26:22 +0100

    pimped the responsive part

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,60 +4,52 @@ import { Link } from "react-router-dom";
 const Welcome = () => {
   return (
     <div className="welcomepage">
-      <header>
-        <img
-          className="welcomelogo"
-          alt="logo"
-          src="./prospa-logo-purple.svg"
-        />
+      {/* <header>
+        <img className="welcomelogo" src="./prospa-logo-purple.svg" />
         <ul>
           <li>Home</li>
           <li>About Us</li>
         </ul>
-      </header>
+      </header> */}
+      <div className="gradientBG">
+        <img className="welcomelogo" src="./prospa-logo.svg" />
+        <h1>Welcome</h1>
+        <p>
+          Thank you for your interest in prospa.
+          <br /> Please find below the documents required for sign up
+        </p>
+        <div className="requirements">
+          <div className="items c">
+            <h2>Bvn</h2>
+            <img className="illustration" src="./bvn.svg" alt="id" />
+            <p>
+              Your 10 digit BVN needed for verification purpose. It enables us
+              to ensure you are the true owner of the account and also follow
+              the Central Bank regulation and directive
+            </p>
+          </div>
+          <div className="items">
+            <h2>Cac</h2>
 
-      <div className="welcome">
-        <div>
-          <h1>Welcome</h1>
-          <p>Thank you for your interest in Prospa.</p>
-          <p> My name is Amaka and I will be assisting</p>
-          <p> you with your sign up. Kindly find the </p>
-          <p> requirements needed below</p>
+            <img className="illustration" src="./CACill.svg" alt="id" />
+            <p>
+              Your CAC document which was issued to you when registering for
+              your business enables ensure that you are the true owner of your
+              business and as such makes the verification easy
+            </p>
+          </div>
+          <div className="items ">
+            <h2>Id Card</h2>
+            <img className="illustration" src="./idcard.svg" alt="id" />
+            <p>
+              A Valid ID Card such as your international passport or National ID
+              Card or Voters Card is needed for individual identification and
+              verification process
+            </p>
+          </div>
         </div>
-        <img className="amaka" alt="amaka" src="./undraw.png" alt="Amaka" />
-      </div>
-      <div className="requirements">
-        <div className="items colored one">
-          <h2>BVN</h2>
-          <hr />
-          <p>Your 10 digit BVN needed for verification purpose</p>
-        </div>
-        <div className="items two">
-          <h2>CAC</h2>
-          <hr />
-          <p>Your 10 digit BVN needed for verification purpose</p>
-        </div>
-        <div className="items colored three">
-          <h2>ID Card</h2>
-          <hr />
-          <p>Your 10 digit BVN needed for verification purpose</p>
-        </div>
-        <div className="items four">
-          <h2>CSS</h2>
-          <hr />
-          <p>Your 10 digit BVN needed for verification purpose</p>
-        </div>
-      </div>
-      <div className="bottomdiv">
-        <p>Got All these items?</p>
-        <button>
-          <Link
-            to="/account"
-            style={{ color: "white", textDecoration: "none" }}
-          >
-            Proceed
-          </Link>
-        </button>
+
+        <button>Proceed</button>
       </div>
     </div>
   );
